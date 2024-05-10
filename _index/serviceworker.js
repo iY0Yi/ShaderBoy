954bf51076cf8c899a82b18e3060ad38f774c590
Atsushi Hashimoto <atsuhashi@gmail.com>
2024-05-10 13:04:20 +0900

    cache nothing
    cache nothing

--- _index/serviceworker.js
+++ _index/serviceworker.js
@@ -4,30 +4,7 @@ self.addEventListener('install', function (event)
     event.waitUntil(
         caches.open('static').then(function (cache)
         {
-            cache.addAll(['/app/',
-                '/app/index.html',
-                '/app/js/main.min.js', 
-                '/app/js/gif.worker.js',
-                '/app/js/CCapture.all.min.js',
-                '/app/shader/_common.vs',
-                '/app/shader/_commonfooter.fs',
-                '/app/shader/_config.json',
-                '/app/shader/_guiknobs.json',
-                '/app/shader/_guitimeline.json',
-                '/app/shader/_setting.json',
-                '/app/shader/_uniform.fs',
-                '/app/shader/_util.fs',
-                '/app/shader/buf_a.fs',
-                '/app/shader/buf_b.fs',
-                '/app/shader/buf_c.fs',
-                '/app/shader/buf_d.fs',
-                '/app/shader/common.fs',
-                '/app/shader/commonfooter.fs',
-                '/app/shader/image.fs',
-                '/app/shader/screen.fs',
-                '/app/shader/shaderlist.js',
-                '/app/shader/writetest.fs',
-                '/app/manifest.json']);
+            cache.addAll([]);
         })
     );
 });
--- dest/serviceworker.js
+++ dest/serviceworker.js
@@ -4,30 +4,7 @@ self.addEventListener('install', function (event)
     event.waitUntil(
         caches.open('static').then(function (cache)
         {
-            cache.addAll(['/',
-                '/index.html',
-                '/js/main.min.js', 
-                '/js/gif.worker.js',
-                '/js/CCapture.all.min.js',
-                '/shader/_common.vs',
-                '/shader/_commonfooter.fs',
-                '/shader/_config.json',
-                '/shader/_guiknobs.json',
-                '/shader/_guitimeline.json',
-                '/shader/_setting.json',
-                '/shader/_uniform.fs',
-                '/shader/_util.fs',
-                '/shader/buf_a.fs',
-                '/shader/buf_b.fs',
-                '/shader/buf_c.fs',
-                '/shader/buf_d.fs',
-                '/shader/common.fs',
-                '/shader/commonfooter.fs',
-                '/shader/image.fs',
-                '/shader/screen.fs',
-                '/shader/shaderlist.js',
-                '/shader/writetest.fs',
-                '/manifest.json']);
+            cache.addAll([]);
         })
     );
 });
