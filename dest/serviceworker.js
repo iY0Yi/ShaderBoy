0431e3d78758a4d90839909f4517579229776166
Atsushi Hashimoto <atsuhashi@gmail.com>
2024-05-14 14:50:38 +0900

    Cache problem on developing.
    Cache problem on developing.

--- dest/serviceworker.js
+++ dest/serviceworker.js
@@ -1,33 +1,38 @@
-self.addEventListener('install', function (event)
-{
+const CACHE_NAME = 'static-v1';
+const DYNAMIC_CACHE_NAME = 'dynamic-v1';
+
+self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static').then(function (cache)
-        {
-            cache.addAll([]);
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([]);
         })
     );
 });
 
-self.addEventListener('activate', function (event)
-{
+self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker ....', event);
+    event.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
+                    console.log('[Service Worker] Removing old cache.', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (event)
-{
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function (response)
-        {
-            if (response)
-            {
+        caches.match(event.request).then(function(response) {
+            if (response) {
                 return response;
-            } else
-            {
-                return fetch(event.request).then(function (res)
-                {
-                    return caches.open('dynamic').then(function (cache)
-                    {
+            } else {
+                return fetch(event.request).then(function(res) {
+                    return caches.open(DYNAMIC_CACHE_NAME).then(function(cache) {
                         cache.put(event.request.url, res.clone());
                         return res;
                     });
