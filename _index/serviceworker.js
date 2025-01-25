9b2e7e65b5e6f3ef6c434677d45611f45f1fea89
Atsushi Hashimoto <atsuhashi@gmail.com>
2025-01-25 17:31:59 +0900

    Stopped using the cache of the service worker.
    Now, you can update the app with super reloading.

--- _index/serviceworker.js
+++ _index/serviceworker.js
@@ -1,43 +1,14 @@
-const CACHE_NAME = 'static-v1';
-const DYNAMIC_CACHE_NAME = 'dynamic-v1';
-
-self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll([]);
-        })
-    );
+self.addEventListener('install', (event) => {
+    console.log('[Service Worker] Installing and skipping waiting...');
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event) {
-    console.log('[Service Worker] Activating Service Worker ....', event);
-    event.waitUntil(
-        caches.keys().then(function(keyList) {
-            return Promise.all(keyList.map(function(key) {
-                if (key !== CACHE_NAME && key !== DYNAMIC_CACHE_NAME) {
-                    console.log('[Service Worker] Removing old cache.', key);
-                    return caches.delete(key);
-                }
-            }));
-        })
-    );
-    return self.clients.claim();
+self.addEventListener('activate', (event) => {
+    console.log('[Service Worker] Activating and claiming clients...');
+    event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request).then(function(res) {
-                    return caches.open(DYNAMIC_CACHE_NAME).then(function(cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                    });
-                });
-            }
-        })
-    );
+self.addEventListener('fetch', (event) => {
+    console.log(`[Service Worker] Fetching: ${event.request.url}`);
+    event.respondWith(fetch(event.request));
 });
--- dest/manup.js
+++ dest/manup.js
@@ -1,133 +1,132 @@
-
-//this is the object we build for an ajax call to get the content of the manifest
-//it's a JSON, so we can parse it like a man-child
-//verion 0.7
+// Manup.js - Enhanced version
 var manUpObject;
 
+// データオブジェクト
+var validMetaValues = [
+    { name: 'mobile-web-app-capable', manifestName: 'display' },
+    { name: 'apple-mobile-web-app-capable', manifestName: 'display' },
+    { name: 'apple-mobile-web-app-title', manifestName: 'short_name' },
+    { name: 'application-name', manifestName: 'short_name' }
+];
 
+var validLinkValues = [
+    { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '152x152' },
+    { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '120x120' },
+    { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '76x76' },
+    { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '60x60' },
+    { name: 'icon', manifestName: 'icons', imageSize: '192x192' },
+    { name: 'icon', manifestName: 'icons', imageSize: '512x512' },
+    { name: 'icon', manifestName: 'icons', imageSize: '620x300' }, // 新規
+    { name: 'icon', manifestName: 'icons', imageSize: '1240x600' } // 新規
+];
 
-//data objects
-var tagArray = [], linkArray = [];
-var validMetaValues = [{ name: 'mobile-web-app-capable', manifestName: 'display' }, { name: 'apple-mobile-web-app-capable', manifestName: 'display' }, { name: 'apple-mobile-web-app-title', manifestName: 'short_name' }, { name: 'application-name', manifestName: 'short_name' }, { name: 'msapplication-TileColor', manifestName: 'ms_TileColor' }, { name: 'msapplication-square70x70logo', manifestName: 'icons', imageSize: '70x70' }, { name: 'msapplication-square150x150logo', manifestName: 'icons', imageSize: '150x150' },{ name: 'msapplication-wide310x150logo', manifestName: 'icons', imageSize: '310x150' },{ name: 'msapplication-square310x310logo', manifestName: 'icons', imageSize: '310x310' }];
-var validLinkValues = [{ name: 'apple-touch-icon', manifestName: 'icons', imageSize: '152x152' },{ name: 'apple-touch-icon', manifestName: 'icons', imageSize: '120x120' },{ name: 'apple-touch-icon', manifestName: 'icons', imageSize: '76x76' },{ name: 'apple-touch-icon', manifestName: 'icons', imageSize: '60x60' },{ name: 'apple-touch-icon', manifestName: 'icons', imageSize: '57x57' }, { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '72x72' }, { name: 'apple-touch-icon', manifestName: 'icons', imageSize: '114x114' }, { name: 'icon', manifestName: 'icons', imageSize: '128x128' }, { name: 'icon', manifestName: 'icons', imageSize: '192x192' }]
-
-
-//these next two classes are building the mixed data, pulling out the values we need based on the valid values array
-var generateFullMetaData = function(){
- for (var i = 0;i < validMetaValues.length;  i++) {
-  if(manUpObject[validMetaValues[i].manifestName]){
-      if(validMetaValues[i].manifestName == 'icons'){
-          //here we need to loop through each of the images to see if they match
-          var imageArray = manUpObject.icons;
-          for (var j = 0;j < imageArray.length;  j++) {
-              if(imageArray[j].sizes == validMetaValues[i].imageSize){
-                //problem is in here, need to asign proper value
-                validMetaValues[i].content = imageArray[j].src;
-              }
-          }
-      }else{
-          validMetaValues[i].content = manUpObject[validMetaValues[i].manifestName];
-          if (validMetaValues[i].manifestName == 'display' && manUpObject.display == 'standalone') validMetaValues[i].content = 'yes'
-         // console.log('stop')
-      }
-  } 
- };
-
- //console.log(validMetaValues)
- return validMetaValues
+// タグの重複チェック
+var isTagExists = function (tagName, attributeName, attributeValue) {
+    const tags = document.querySelectorAll(`${tagName}[${attributeName}="${attributeValue}"]`);
+    return tags.length > 0;
 };
 
-var generateFullLinkData = function () {
-    for (var i = 0; i < validLinkValues.length; i++) {
-        if (manUpObject[validLinkValues[i].manifestName]) {
-            if (validLinkValues[i].manifestName == 'icons') {
-                //here we need to loop through each of the images to see if they match
-                var imageArray = manUpObject.icons;
-                for (var j = 0; j < imageArray.length; j++) {
-                    if (imageArray[j].sizes == validLinkValues[i].imageSize) {
-                        //problem is in here, need to asign proper value
-                        validLinkValues[i].content = imageArray[j].src;
-                    }
-                }
-
+// メタデータ生成
+var generateFullMetaData = function () {
+    validMetaValues.forEach(meta => {
+        if (manUpObject[meta.manifestName]) {
+            if (meta.manifestName === 'display' && manUpObject.display === 'standalone') {
+                meta.content = 'yes';
             } else {
-
-                validLinkValues[i].content = manUpObject[validLinkValues[i].manifestName];
+                meta.content = manUpObject[meta.manifestName];
             }
         }
-    };
-
-    //console.log(validMetaValues)
-    return validLinkValues
+    });
+    return validMetaValues;
 };
 
+// リンクデータ生成
+var generateFullLinkData = function () {
+    validLinkValues.forEach(link => {
+        if (manUpObject[link.manifestName]) {
+            if (link.manifestName === 'icons') {
+                var imageArray = manUpObject.icons || [];
+                imageArray.forEach(image => {
+                    if (image.sizes === link.imageSize) {
+                        link.content = image.src;
+                        if (image.purpose) {
+                            link.purpose = image.purpose; // purpose を追加
+                        }
+                    }
+                });
+            } else {
+                link.content = manUpObject[link.manifestName];
+            }
+        }
+    });
+    return validLinkValues;
+};
 
-        //These are the 2 functions that build out the tags
-        //TODO: make it loop once instead of tx
+// メタタグ生成
 var generateMetaArray = function () {
     var tempMetaArray = generateFullMetaData();
-    var headTarget = document.getElementsByTagName('head')[0]
-    for (var i = 0; i < tempMetaArray.length; i++) {
-
-        var metaTag = document.createElement('meta');
-        metaTag.name = tempMetaArray[i].name;
-        metaTag.content = tempMetaArray[i].content;
-        headTarget.appendChild(metaTag);
-
-    };
-
+    var headTarget = document.getElementsByTagName('head')[0];
+    tempMetaArray.forEach(meta => {
+        if (!isTagExists('meta', 'name', meta.name)) {
+            var metaTag = document.createElement('meta');
+            metaTag.name = meta.name;
+            metaTag.content = meta.content;
+            headTarget.appendChild(metaTag);
+        }
+    });
 };
 
+// リンクタグ生成
 var generateLinkArray = function () {
     var tempLinkArray = generateFullLinkData();
-    var headTarget = document.getElementsByTagName('head')[0]
-    for (var i = 0; i < tempLinkArray.length; i++) {
-
-        var linkTag = document.createElement('link');
-        linkTag.setAttribute('rel', tempLinkArray[i].name);
-        linkTag.setAttribute('sizes', tempLinkArray[i].imageSize);
-        linkTag.setAttribute('href', tempLinkArray[i].content);
-        headTarget.appendChild(linkTag);
-        //console.log(linkTag);
-    }
+    var headTarget = document.getElementsByTagName('head')[0];
+    tempLinkArray.forEach(link => {
+        if (!isTagExists('link', 'rel', link.name)) {
+            var linkTag = document.createElement('link');
+            linkTag.setAttribute('rel', link.name);
+            linkTag.setAttribute('sizes', link.imageSize);
+            linkTag.setAttribute('href', link.content);
+            if (link.purpose) {
+                linkTag.setAttribute('purpose', link.purpose); // purpose 属性を追加
+            }
+            headTarget.appendChild(linkTag);
+        }
+    });
 };
-//these functions makes the ajax calls and assigns to manUp object
+
+// Manifest のデータ生成
 var generateObj = function (ajaxString) {
-    //for testing
-    //document.getElementById("output").innerHTML = ajaxString;
-    //gen object
     manUpObject = JSON.parse(ajaxString);
     generateLinkArray();
     generateMetaArray();
-
 };
 
+// Ajax を fetch に置き換え
 var makeAjax = function (url) {
-    if (!window.XMLHttpRequest) return;
-    var fullURL;
-    var pat = /^https?:\/\//i;
-    pat.test(url)?fulURL = url:fullURL = window.location.hostname + url;
-    var ajax = new XMLHttpRequest();   
-    ajax.onreadystatechange = function () {
-        if (ajax.readyState == 4 && ajax.status == 200) generateObj(ajax.responseText)
-    };
-    ajax.open("GET", url, true);
-    ajax.send();
+    fetch(url)
+        .then(response => {
+            if (response.ok) return response.text();
+            throw new Error('Failed to load manifest');
+        })
+        .then(generateObj)
+        .catch(console.error);
 };
 
+// Manifest を取得
 var collectManifestObj = function () {
     var links = document.getElementsByTagName('link');
     for (var i = 0; i < links.length; i++) {
-        if (links[i].rel && links[i].rel == 'manifest') makeAjax(links[i].href);
+        if (links[i].rel && links[i].rel === 'manifest') {
+            makeAjax(links[i].href);
+        }
     }
-
 };
 
-var testForManifest = function () {
-    //i'm not sure what to do here.  I am starchly against browser sniffing and there is no test (yet)
-    //I think I'm jsut going to build out the tags on every page until there is a way to test it, so far
-    //it looks like the manifest will override the tags
-    //browser: we need context here
-    collectManifestObj();
+// PWA 対応ブラウザを判定して処理を実行
+var isPWASupported = function () {
+    return 'serviceWorker' in navigator && 'PushManager' in window;
+};
 
-}();
+if (!isPWASupported()) {
+    collectManifestObj();
+}
